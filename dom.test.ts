c8d291d9f1e393dcacafe6ec6cf6e9f6f62b5064
balesniy <balesniy@yandex.ru>
2023-11-10 21:50:53 +0100

    update test

--- dom.test.ts
+++ dom.test.ts
@@ -8,7 +8,6 @@ const PORT = 3000
 describe('basic text styles', async () => {
     let server: PreviewServer
     let browser: Browser
-    let page: Page
 
     beforeAll(async () => {
         server = await preview({
@@ -22,8 +21,6 @@ describe('basic text styles', async () => {
             configFile: false,
         })
         browser = await launch({headless: true})
-        page = await browser.newPage()
-        await page.goto(`http://localhost:${PORT}`, {waitUntil: 'networkidle0'})
     })
 
     afterAll(async () => {
@@ -37,125 +34,155 @@ describe('basic text styles', async () => {
         return $container.$$(`xpath///*[contains(text(), '${text}')]`)
     }
 
-    const runTest = async (testName: string, elements: string[]) => {
-        const $container = await page.$(`[data-test="${testName}"]`);
-        for (const text of elements) {
-            const [$el] = await getByText($container, text);
-            if (!$el) {
-                throw new Error(`Element with text "${text}" not found`);
-            }
-            const styleProperties = await page.evaluate(el => {
-                const cssObj = getComputedStyle(el);
-                const getProperties = (cssObj, properties) => {
-                    return properties.reduce((acc, curr) => {
-                        acc[curr] = cssObj.getPropertyValue(curr);
-                        return acc;
-                    }, {});
-                };
-                return getProperties(cssObj, ['font-size', 'font-family', 'line-height', 'font-weight', 'letter-spacing', 'color']);
-            }, $el);
-            expect(styleProperties).toMatchSnapshot();
+
+    type Test = { name: string, parameters: string[] };
+
+    const tests: Test[] = [
+        {
+            name: 'hero',
+            parameters: ['Омск', '8-800-555-55-55', 'Тренажёрный зал', 'Купить абонемент']
+        },
+        {
+            name: 'about',
+            parameters: ['Тренажерный зал', 'Просторный зал', 'самый большой фитнес центр']
+        },
+        {
+            name: 'price',
+            parameters: [
+                'Абонементы',
+                '1 месяц',
+                '6 месяцев',
+                'С тренером',
+                '12 занятий',
+                'Купить абонемент',
+                'Дневной',
+                'с 8:00 до 17:00'
+            ]
+        },
+        {
+            name: 'games',
+            parameters: [
+                'Super games',
+                'Дата',
+                '7',
+                'Марта 2019',
+                'Время',
+                '12:00',
+                'Заполнить заявку',
+                'Ежегодные соревнования',
+                'Жюри',
+                'Анна Павлова',
+                'Certified',
+                'Победитель чемпионата России',
+                'Опыт'
+            ]
+        },
+        {
+            name: 'features',
+            parameters: [
+                'Наши преимущества',
+                '900',
+                'кв/м',
+                'Площадь',
+                'Занимайтесь без',
+                '100',
+                'Тренажеров',
+                'Современные тренажеры',
+            ]
+        },
+        {
+            name: 'offers',
+            parameters: [
+                'Акции',
+                'Год',
+                '4999',
+                'Безлимитный абонемент',
+                'Подробнее',
+                'Месяц бесплатно',
+                'Приведи друга',
+                'Скидка',
+                'Корпоративный фитнес'
+            ]
+        },
+        {
+            name: 'faq',
+            parameters: [
+                'Вопросы и ответы',
+                'Центр',
+                'Абонемент',
+                'Как стать членом',
+                'При первом посещении',
+                'Где можно посмотреть',
+                'Какие дополнительные правила',
+                'Для тренировок необходимо',
+            ]
+        },
+        {
+            name: 'reviews',
+            parameters: [
+                'Отзывы',
+                'Анна Орлова',
+                'больше года',
+            ]
+        },
+        {
+            name: 'footer',
+            parameters: [
+                'Бесплатное занятие',
+                'Отправить',
+                'Контакты',
+                'Адрес',
+                '60 лет Октября',
+                'График работы:',
+                'Пн-Вс: с 8:00 до 22:00',
+                'Телефон:',
+                '8-800-555-55-55',
+                'Email:',
+                'omsk@supergym.ru',
+                'Услуги',
+                'Абонементы',
+            ]
         }
-    };
+    ];
 
-    test('hero', async () => {
-        await runTest('hero', ['Омск', '8-800-555-55-55', 'Тренажёрный зал', 'Купить абонемент']);
-    });
+    async function runViewportTest(viewport: { width: number, height: number }, tests: Test[]) {
+        let page: Page;
+        beforeAll(async () => {
+            page = await browser.newPage()
+            await page.setViewport(viewport);
+            await page.goto(`http://localhost:${PORT}`, {waitUntil: 'networkidle0'})
+        });
+        for (const { name: testName, parameters: elements } of tests) {
+            test(testName, async () => {
+                const $container = await page.$(`[data-test="${testName}"]`);
+                for (const text of elements) {
+                    const [$el] = await getByText($container, text);
+                    if (!$el) {
+                        throw new Error(`Element with text "${text}" not found`);
+                    }
+                    const styleProperties = await page.evaluate(el => {
+                        const cssObj = getComputedStyle(el);
+                        const getProperties = (cssObj, properties) => {
+                            return properties.reduce((acc, curr) => {
+                                acc[curr] = cssObj.getPropertyValue(curr);
+                                return acc;
+                            }, {});
+                        };
+                        return getProperties(cssObj, ['font-size', 'font-family', 'line-height', 'font-weight', 'letter-spacing', 'color']);
+                    }, $el);
+                    expect({...styleProperties, text}).toMatchSnapshot();
+                }
+            });
+        }
+    }
 
-    test('about', async () => {
-        await runTest('about', ['Тренажерный зал', 'Просторный зал', 'самый большой фитнес центр']);
-    });
-    test('price', async () => {
-        await runTest('price', [
-            'Абонементы',
-            '1 месяц',
-            '6 месяцев',
-            'С тренером',
-            '12 занятий',
-            'Купить абонемент',
-            'Дневной',
-            'с 8:00 до 17:00'
-        ]);
-    });
-    test('games', async () => {
-        await runTest('games', [
-            'Super games',
-            'Дата',
-            '7',
-            'Марта 2019',
-            'Время',
-            '12:00',
-            'Заполнить заявку',
-            'Ежегодные соревнования',
-            'Жюри',
-            'Анна Павлова',
-            'Certified',
-            'Победитель чемпионата России',
-            'Опыт',
-
-        ]);
-    });
-    test('features', async () => {
-        await runTest('features', [
-            'Наши преимущества',
-            '900',
-            'кв/м',
-            'Площадь',
-            'Занимайтесь без',
-            '100',
-            'Тренажеров',
-            'Современные тренажеры',
-        ]);
-    });
-    test('offers', async () => {
-        await runTest('offers', [
-            'Акции',
-            'Год',
-            '4999',
-            'Безлимитный абонемент',
-            'Подробнее',
-            'Месяц бесплатно',
-            'Приведи друга',
-            'Скидка',
-            'Корпоративный фитнес',
-            ''
-        ]);
+    describe('desktop', async () => {
+        await runViewportTest({width: 1600, height: 800}, tests);
     })
-    test('faq', async () => {
-        await runTest('faq', [
-            'Вопросы и ответы',
-            'Центр',
-            'Абонемент',
-            'Как стать членом',
-            'При первом посещении',
-            'Где можно посмотреть',
-            'Какие дополнительные правила',
-            'Для тренировок необходимо',
-        ]);
-    });
-    test('reviews', async () => {
-        await runTest('reviews', [
-            'Отзывы',
-            'Анна Орлова',
-            'больше года',
-        ]);
-    });
-    test('footer', async () => {
-        await runTest('footer', [
-            'Бесплатное занятие',
-            'Отправить',
-            'Контакты',
-            'Адрес',
-            '60 лет Октября',
-            'График работы:',
-            'Пн-Вс: с 8:00 до 22:00',
-            'Телефон:',
-            '8-800-555-55-55',
-            'Email:',
-            'omsk@supergym.ru',
-            'Услуги',
-            'Абонементы',
-            ''
-        ]);
-    });
+    describe('tablet', async () => {
+        await runViewportTest({width: 1024, height: 800}, tests);
+    })
+    describe('mobile', async () => {
+        await runViewportTest({width: 375, height: 800}, tests);
+    })
 })
